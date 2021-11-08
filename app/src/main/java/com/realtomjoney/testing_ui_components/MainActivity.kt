ffa6886c7641a87864ca274e3fd14372f5d910d1
xTheCoderx <justaguyoney@gmail.com>
2021-11-08 18:19:17 +1300

    Setup MainActivity.kt to utilise view binding.

--- app/src/main/java/com/realtomjoney/testing_ui_components/MainActivity.kt
+++ app/src/main/java/com/realtomjoney/testing_ui_components/MainActivity.kt
@@ -2,10 +2,18 @@ package com.realtomjoney.testing_ui_components
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.realtomjoney.testing_ui_components.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun setBindings() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
