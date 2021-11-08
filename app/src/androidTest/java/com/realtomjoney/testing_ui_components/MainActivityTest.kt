19e9a07fc2168b9bd111c6604d61475af5bb47b7
xTheCoderx <justaguyoney@gmail.com>
2021-11-08 18:40:40 +1300

    Removed unused import directives in MainActivityTest.kt.

--- app/src/androidTest/java/com/realtomjoney/testing_ui_components/MainActivityTest.kt
+++ app/src/androidTest/java/com/realtomjoney/testing_ui_components/MainActivityTest.kt
@@ -1,8 +1,6 @@
 package com.realtomjoney.testing_ui_components
 
 
-import android.view.View
-import android.view.ViewGroup
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -10,10 +8,6 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
-import org.hamcrest.Description
-import org.hamcrest.Matcher
-import org.hamcrest.Matchers.allOf
-import org.hamcrest.TypeSafeMatcher
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
