d0a2562314ad667195fe46a3723a52f1fe846689
xTheCoderx <justaguyoney@gmail.com>
2021-11-08 18:20:20 +1300

    Added basic 'setOnClickListener' code in MainActivity.kt for the button in activity_main.xml.

--- app/src/main/java/com/realtomjoney/testing_ui_components/MainActivity.kt
+++ app/src/main/java/com/realtomjoney/testing_ui_components/MainActivity.kt
@@ -10,6 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setBindings()
+
+        binding.button.setOnClickListener {
+            binding.textView.text = "Kotlin"
+        }
     }
 
     private fun setBindings() {
